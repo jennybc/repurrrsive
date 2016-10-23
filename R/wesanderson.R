ecce3c688187b0c1f67889bc7fb2a65495089a0c
jennybc <jenny@stat.ubc.ca>
2016-10-22 22:56:40 -0700

    add wesanderson palettes

---
+++ R/wesanderson.R
@@ -0,0 +1,14 @@
+#' Color palettes from Wes Anderson movies
+#'
+#' A list of color palettes inspried by Wes Anderson movies, taken from the from
+#' \code{\link{wesanderson}} package.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{https://cran.r-project.org/package=wesanderson}
+#' @source \url{http://wesandersonpalettes.tumblr.com}
+"wesanderson"
---
+++ data-raw/prep_wesanderson.R
@@ -0,0 +1,33 @@
+## devtools::install_github("krlmlr/here")
+library(here)
+library(wesanderson)
+library(jsonlite)
+library(xml2)
+#library(xmlview)
+library(tidyverse)
+
+wesanderson <- wes_palettes
+use_data(wesanderson, overwrite = TRUE)
+
+list(wesanderson = wes_palettes) %>%
+  toJSON() %>%
+  prettify() %>%
+  writeLines(con = here("inst", "extdata", "wesanderson.json"))
+
+wes_df <- enframe(wes_palettes)
+
+wes_xml <- xml_new_document() %>%
+  xml_add_child("package", name = "wesanderson") %>%
+  xml_root()
+#xml_view(wes_xml)
+wes_df$name %>%
+  walk(~ xml_add_child(wes_xml, "palette", name = .x))
+#xml_view(wes_xml)
+f <- function(parent, kids) {
+  map(kids, ~ xml_add_child(parent, "color", hex = .x))
+}
+map2(xml_children(wes_xml), wes_df$value, ~ f(.x, .y))
+#xml_view(wes_xml)
+write_xml(wes_xml,
+          here("inst", "extdata", "wesanderson.xml"))
+
