b75c4680570ccb874fa50ad7cd8449b9f111da0c
jennybc <jenny@stat.ubc.ca>
2016-10-23 13:30:00 -0700

    put all wesanderson stuff in one file

--- R/wesanderson.R
+++ R/wesanderson.R
@@ -12,3 +12,31 @@
 #' @source \url{https://cran.r-project.org/package=wesanderson}
 #' @source \url{http://wesandersonpalettes.tumblr.com}
 "wesanderson"
+
+#' Path to wesanderson JSON
+#'
+#' @return Local path to JSON file containing Wes Anderson color palettes
+#' @export
+#'
+#' @examples
+#' wesanderson_json()
+#' if (require("jsonlite")) {
+#' jsonlite::fromJSON(wesanderson_json())
+#' }
+wesanderson_json <- function() {
+  system.file("extdata", "wesanderson.json", package = "repurrrsive")
+}
+
+#' Path to wesanderson XML
+#'
+#' @return Local path to XML file containing Wes Anderson color palettes
+#' @export
+#'
+#' @examples
+#' wesanderson_xml()
+#' if (require("xml2")) {
+#' read_xml(wesanderson_xml())
+#' }
+wesanderson_xml <- function() {
+  system.file("extdata", "wesanderson.xml", package = "repurrrsive")
+}
