fa233217a834ed07194388260997424a83a7adf1
jennybc <jenny@stat.ubc.ca>
2016-10-24 16:16:44 -0700

    tighten up XML authoring code

--- data-raw/prep_wesanderson.R
+++ data-raw/prep_wesanderson.R
@@ -1,10 +1,11 @@
 ## devtools::install_github("krlmlr/here")
 library(here)
 library(wesanderson)
+library(devtools)
 library(jsonlite)
 library(xml2)
 #library(xmlview)
-library(tidyverse)
+library(purrr)
 
 wesanderson <- wes_palettes
 use_data(wesanderson, overwrite = TRUE)
@@ -12,21 +13,35 @@ use_data(wesanderson, overwrite = TRUE)
 list(wesanderson = wes_palettes) %>%
   toJSON() %>%
   prettify() %>%
-  writeLines(con = here("inst", "extdata", "wesanderson.json"))
-
-wes_df <- enframe(wes_palettes)
+  writeLines(here("inst", "extdata", "wesanderson.json"))
 
 wes_xml <- xml_new_document() %>%
   xml_add_child("package", name = "wesanderson") %>%
   xml_root()
 #xml_view(wes_xml)
-wes_df$name %>%
-  walk(~ xml_add_child(wes_xml, "palette", name = .x))
+walk2(names(wes_palettes), wes_palettes, function(pal_name, pal_cols) {
+  pal_node <- xml_add_child(wes_xml, "palette", name = pal_name)
+  map(pal_cols, ~ xml_add_child(pal_node, "hex", .x))
+})
 #xml_view(wes_xml)
-f <- function(parent, kids) {
-  map(kids, ~ xml_add_child(parent, "hex", .x))
-}
-map2(xml_children(wes_xml), wes_df$value, ~ f(.x, .y))
-#xml_view(wes_xml)
+
 write_xml(wes_xml,
           here("inst", "extdata", "wesanderson.xml"))
+
+## my personal XML journey
+# wes_xml2 <- xml_new_document() %>%
+#   xml_add_child("package", name = "wesanderson") %>%
+#   xml_root()
+# xml_view(wes_xml2)
+
+## install parents (= palette nodes)
+# names(wesanderson) %>%
+#   walk(~ xml_add_child(wes_xml2, "palette", name = .x))
+#  xml_view(wes_xml)
+
+## install children (= colors inside palettes)
+# f <- function(parent, kids) {
+#   map(kids, ~ xml_add_child(parent, "hex", .x))
+# }
+# walk2(xml_children(wes_xml), wesanderson, ~ f(.x, .y))
+# xml_view(wes_xml)
