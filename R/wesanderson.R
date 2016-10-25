afb9e98a2cd0322246a9f96ecbf36676c3b3f1f0
jennybc <jenny@stat.ubc.ca>
2016-10-24 23:17:17 -0700

    work on wesanderson help

--- R/wesanderson.R
+++ R/wesanderson.R
@@ -1,41 +1,40 @@
 #' Color palettes from Wes Anderson movies
 #'
-#' A list of color palettes inspried by Wes Anderson movies, taken from the from
+#' A list of color palettes inspired by Wes Anderson movies, taken from the from
 #' \code{\link{wesanderson}} package.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
+#' @format A named list with 15 components, each containing a color palette from
+#'   a specific movie. Each palette consists of 4 or 5 hexadecimal color values.
+#'
 #' @source \url{https://cran.r-project.org/package=wesanderson}
 #' @source \url{http://wesandersonpalettes.tumblr.com}
+#' @family wesanderson data and functions
+#' @examples
+#' wesanderson[4:5]
+#' wesanderson[["Rushmore"]]
 "wesanderson"
 
-#' Path to wesanderson JSON
+#' Path to wesanderson JSON and XML
 #'
-#' @return Local path to JSON file containing Wes Anderson color palettes
+#' @return Local path to JSON or XML file containing Wes Anderson color palettes
 #' @export
 #'
+#' @family wesanderson data and functions
 #' @examples
 #' wesanderson_json()
 #' if (require("jsonlite")) {
-#' jsonlite::fromJSON(wesanderson_json())
+#'   jsonlite::fromJSON(wesanderson_json())
 #' }
 wesanderson_json <- function() {
   system.file("extdata", "wesanderson.json", package = "repurrrsive")
 }
 
-#' Path to wesanderson XML
-#'
-#' @return Local path to XML file containing Wes Anderson color palettes
 #' @export
-#'
+#' @rdname wesanderson_json
 #' @examples
 #' wesanderson_xml()
 #' if (require("xml2")) {
-#' read_xml(wesanderson_xml())
+#'   xml2::read_xml(wesanderson_xml())
 #' }
 wesanderson_xml <- function() {
   system.file("extdata", "wesanderson.xml", package = "repurrrsive")
