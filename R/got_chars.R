f2098904c93dfadbd49a6746438389b15566989e
Stu Field <stu.g.field@gmail.com>
2019-01-24 14:42:30 -0700

    Update documentation for got_chars length 29 -> 30 and each entry within being length 16 -> 18. Rerun roxygen2 to update *.Rd files.
    Fixes #13

--- R/got_chars.R
+++ R/got_chars.R
@@ -4,8 +4,8 @@
 #' Song of Ice and Fire series by George R. R. Martin. Retrieved from An API Of
 #' Ice And Fire.
 #'
-#' @format A unnamed list with 29 components, each representing a POV character.
-#'   Each character's component is a named list of length 16, containing
+#' @format A unnamed list with 30 components, each representing a POV character.
+#'   Each character's component is a named list of length 18, containing
 #'   information such as name, aliases, and house allegiances.
 #'
 #' @family Game of Thrones data and functions
