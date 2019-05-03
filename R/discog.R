7ecc79f9a6b1925996ec20d78f0291d91451db31
Hadley Wickham <h.wickham@gmail.com>
2019-05-03 11:08:18 +1000

    Update discography (#15)

---
+++ R/discog.R
@@ -0,0 +1,10 @@
+#' Sharla Gelfand's music collection
+#'
+#' Inspired by <https://sharla.party/posts/discog-purrr/>.
+#' Retrieved on 2019-05-02 from <discogs.com>.
+#'
+#' @format A unnamed list with 155 components, each representing an item in
+#'   Sharla's music collection.
+#' @examples
+#' length(discog)
+"discog"
