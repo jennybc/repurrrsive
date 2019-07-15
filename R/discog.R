a29df5b15279519d3967c0674244bb76d901b2ef
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-07-15 08:15:18 +0200

    Add JSON path function for discog; more docs

--- R/discog.R
+++ R/discog.R
@@ -1,13 +1,39 @@
 #' Sharla Gelfand's music collection
 #'
-#' Inspired by <https://sharla.party/posts/discog-purrr/>.
+#' A music collection, as represented in a recursive list returned from the
+#' Discogs API.
 #'
 #' @format A unnamed list with 155 components, each representing an item in
 #'   Sharla's music collection.
 #'
-#' @source Retrieved on 2019-07-15 from <https://www.discogs.com>.
+#' @source
+#'   * Data retrieved on 2019-07-15 from <https://www.discogs.com>
+#'   * Original blog post by Sharla Gelfand
+#'   <https://sharla.party/posts/discog-purrr/>
+#'
+#' @family Discogs data and functions
+#'
 #' @examples
 #' length(discog)
 #'
 #' str(discog, max.level = 2, list.len = 2)
+#'
+#' vapply(discog[1:6], `[[`, c("basic_information", "title"), FUN.VALUE = "")
 "discog"
+
+#' Path to Discogs data as JSON
+#'
+#' @return Local path to JSON file containing Discogs data
+#' @export
+#'
+#' @family Discogs data and functions
+#'
+#' @examples
+#' discog_json()
+#' if (require("jsonlite")) {
+#'   d <- fromJSON(discog_json(), simplifyVector = FALSE)
+#'   identical(discog, d)
+#' }
+discog_json <- function() {
+  system.file("extdata", "discog.json", package = "repurrrsive")
+}
